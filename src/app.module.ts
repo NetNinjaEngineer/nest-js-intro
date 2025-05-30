5ae9fa17f566e0f706398e1d115e01f87ad97f73
NetNinjaEngineer <memo5260287@outlook.com>
2025-05-30 18:22:46 +0300

    #custom config file

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,26 +11,28 @@ import { MessagesModule } from './messages/messages.module';
 import { ProfileModule } from './profile/profile.module';
 import { HashtagModule } from './hashtag/hashtags.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { appConfig } from './config/app.config';
 
 const environment = process.env.NODE_ENV;
 @Module({
     imports: [
         ConfigModule.forRoot({
             isGlobal: true,
-            envFilePath: !environment ? '.env' : `.env.${environment.trim()}`
+            envFilePath: !environment ? '.env' : `.env.${environment.trim()}`,
+            load: [appConfig]
         }),
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
             inject: [ConfigService],
             useFactory: (configService: ConfigService) => ({
-                type: configService.get('DB_TYPE') as any,
-                host: configService.get<string>('DB_HOST'),
-                port: parseInt(configService.get<string>('DB_PORT') || '5432', 10),
-                username: configService.get<string>('DB_USERNAME'),
-                password: configService.get<string>('DB_PASSWORD'),
-                database: configService.get<string>('DB_NAME'),
-                autoLoadEntities: true,
-                synchronize: true
+                type: configService.get('database.type') as any,
+                host: configService.get('database.host'),
+                port: +configService.get('database.port'),
+                username: configService.get('database.username'),
+                password: configService.get('database.password'),
+                database: configService.get('database.name') as any,
+                autoLoadEntities: configService.get("database.autoLoadEntities"),
+                synchronize: configService.get("database.synchronize")
             })
         }),
         UsersModule,
